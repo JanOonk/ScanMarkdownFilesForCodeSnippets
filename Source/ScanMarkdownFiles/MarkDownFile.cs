ddd4bac8650bfe19774f4dca1623aca81f87a619
Jan Oonk <J.Oonk@fontys.nl>
2022-01-26 14:22:12 +0100

    initial version

---
+++ Source/ScanMarkdownFiles/MarkDownFile.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanMarkdownFiles
+{
+    internal class MarkDownFile
+    {
+        public string Filename { get; set; }
+        public int CodeSnippetsCount => CountPerCodeSnippet.Sum(codeSnippet => codeSnippet.Value) / 2;
+        public int EmptyCodeSnippetsCount => CodeSnippetsCount - CountPerCodeSnippet
+            .Where(codeSnippet => codeSnippet.Key != "```")
+            .Sum(codeSnippet => codeSnippet.Value);
+        public List<string> CodeSnippetTypes
+        {
+            get
+            {
+                List<string> result = CountPerCodeSnippet.Keys.ToList();
+                result.Sort();
+                return result;
+            }
+        }
+
+        public List<string> NonEmptyCodeSnippetTypes
+        {
+            get
+            {
+                List<string> result = CountPerCodeSnippet
+                    .Keys
+                    .Where(key => key != "```")
+                    .ToList();
+                result.Sort();
+                return result;
+            }
+        }
+
+        public int CodeSnippetTypesCount => CodeSnippetTypes.Count();
+        //a Markdown codeSnippet starts with:
+        // ```
+        // ```json
+        // ```console
+        // ```shell
+        //
+        // and always ends with ```
+
+        //Number of occurences for each codeSnippet type
+        public Dictionary<string, int> CountPerCodeSnippet { get; } = new Dictionary<string, int>();
+
+        public MarkDownFile(string filename)
+        {
+            Filename = filename;
+        }
+
+        public MarkDownFile(string filename, string codeSnippet)
+        {
+            Filename = filename;
+            AddCodeSnippet(codeSnippet);
+        }
+
+        public MarkDownFile(string filename, List<string> codeSnippets)
+        {
+            Filename = filename;
+            AddCodeSnippets(codeSnippets);
+        }
+
+        public void AddCodeSnippets(List<string> codeSnippets)
+        {
+            foreach (string codeSnippet in codeSnippets)
+            {
+                AddCodeSnippet(codeSnippet);
+            }
+        }
+
+        public void AddCodeSnippet(string codeSnippet)
+        {
+            codeSnippet = codeSnippet.ToLower();
+            if (CountPerCodeSnippet.ContainsKey(codeSnippet))
+            {
+                CountPerCodeSnippet[codeSnippet] += 1;
+            }
+            else
+            {
+                CountPerCodeSnippet.Add(codeSnippet, 1);
+            }
+        }
+
+        public bool Validate()
+        {
+            return true;// CodeSnippetsCount != count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{Filename}: {CodeSnippetsCount} codesnippets found");
+            if (CodeSnippetsCount > 0)
+            {
+                if (EmptyCodeSnippetsCount == 0)
+                {
+                    sb.Append($"\n  {CodeSnippetTypesCount-1} Unique Types: {String.Join(", ", NonEmptyCodeSnippetTypes)}");
+                }
+                else
+                {
+                    sb.Append($"\n  {CodeSnippetTypesCount} Unique Types: {String.Join(", ", CodeSnippetTypes)}");
+                }
+                foreach (string codeSnippetType in CodeSnippetTypes)
+                {
+                    string key = codeSnippetType;
+                    int value;
+                    if (key == "```")
+                    {
+                        value = EmptyCodeSnippetsCount;
+                        if (value == 0) continue;
+                    }
+                    else value = CountPerCodeSnippet[key];
+                    sb.Append($"\n  {key}: {value}x");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Source/ScanMarkdownFiles/Program.cs
@@ -0,0 +1,56 @@
+﻿using ScanMarkdownFiles;
+
+//string root = "/temp/documentation";
+//string root = "/temp/goshimmer-docs-update-docs-annotations/documentation";
+//string root = "/temp/goshimmer-docs-update-docs-annotations";
+string root = "/temp/goshimmer-docs-update-docs-annotations v2";
+//string pattern = "send_tr*.md";
+string pattern = "*.md";
+List<string> fileNames = Directory
+    .GetFiles(root, pattern, SearchOption.AllDirectories)
+    .ToList();
+
+Console.WriteLine($"Found {fileNames.Count()} files matching {pattern} in {root}");
+
+List<MarkDownFile> markDownFiles = new List<MarkDownFile>();
+foreach (var filename in fileNames)
+{
+    List<string> lines = File.ReadAllLines(filename).ToList();
+
+    int count = lines.Count(line => line.Contains("```"));
+    List<string> codeSnippets = lines
+        .Select(line => line.Trim())
+        .Where(line => line.StartsWith("```"))
+        .ToList();
+
+    MarkDownFile markDownFile = new MarkDownFile(filename.Remove(0, root.Length), codeSnippets);
+    Console.WriteLine(markDownFile);
+
+    if (markDownFile.CodeSnippetsCount * 2 != count)
+    {
+        throw new Exception("One or more lines having ``` which are NOT at the beginning of the line!");
+    }
+
+    markDownFile.Validate();
+
+    markDownFiles.Add(markDownFile);
+}
+
+var stats = markDownFiles
+    .SelectMany(markDownFile => markDownFile.CountPerCodeSnippet)
+    .GroupBy(countPerCodeSnippet => countPerCodeSnippet.Key)
+    .Select(codeSnippet => new
+    {
+        CodeSnippetType = codeSnippet.Key,
+        TotalCount = codeSnippet.Sum(count => count.Value),
+        NumFilesOccurences = codeSnippet.Count(x => x.Value > 0)
+    })
+    .ToList();
+
+Console.WriteLine("Overall stats:");
+Console.WriteLine($" Unique codesnippet types found: {stats.Count}");
+string statsAsString = " " + String.Join("\n  ", stats
+    .Select(stat => $"{stat.TotalCount, 4}x found in {stat.NumFilesOccurences, 3} file(s): {stat.CodeSnippetType}"));
+
+Console.WriteLine($" {statsAsString}");
+
