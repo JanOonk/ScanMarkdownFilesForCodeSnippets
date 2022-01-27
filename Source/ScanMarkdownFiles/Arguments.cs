a13855ff16c0d3086aa8610538a42c8116f99d2f
Jan Oonk <J.Oonk@fontys.nl>
2022-01-27 09:45:58 +0100

    update
    -build script
    -commandline arguments added

---
+++ Source/ScanMarkdownFiles/Arguments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanMarkdownFiles
+{
+    public class Arguments
+    {
+        public string RootFolder { get; set; }
+        public string FilePattern { get; set; } = "*.md";
+        public bool OnlyToplevel { get; set; } = false;
+    }
+}
--- Source/ScanMarkdownFiles/Program.cs
+++ Source/ScanMarkdownFiles/Program.cs
@@ -1,16 +1,23 @@
 ﻿using ScanMarkdownFiles;
 
-//string root = "/temp/documentation";
-//string root = "/temp/goshimmer-docs-update-docs-annotations/documentation";
-//string root = "/temp/goshimmer-docs-update-docs-annotations";
-string root = "/temp/goshimmer-docs-update-docs-annotations v2";
-//string pattern = "send_tr*.md";
-string pattern = "*.md";
-List<string> fileNames = Directory
-    .GetFiles(root, pattern, SearchOption.AllDirectories)
-    .ToList();
+Arguments arguments;
 
-Console.WriteLine($"Found {fileNames.Count()} files matching {pattern} in {root}");
+//use command line for arguments
+arguments = ParseArguments(args);
+
+//test arguments
+//arguments = new Arguments()
+//{
+//    RootFolder = "/temp/goshimmer-docs-update-docs-annotations v2",
+//    FilePattern = "*.md",
+//    OnlyToplevel = false
+//};
+
+List<string> fileNames = Directory
+.GetFiles(arguments.RootFolder, arguments.FilePattern, (arguments.OnlyToplevel ? SearchOption.TopDirectoryOnly : SearchOption.AllDirectories))
+.ToList();
+
+Console.WriteLine($"Found {fileNames.Count()} files matching {arguments.FilePattern} in {arguments.RootFolder}");
 
 List<MarkDownFile> markDownFiles = new List<MarkDownFile>();
 foreach (var filename in fileNames)
@@ -23,7 +30,7 @@ foreach (var filename in fileNames)
         .Where(line => line.StartsWith("```"))
         .ToList();
 
-    MarkDownFile markDownFile = new MarkDownFile(filename.Remove(0, root.Length), codeSnippets);
+    MarkDownFile markDownFile = new MarkDownFile(filename.Remove(0, arguments.RootFolder.Length), codeSnippets);
     Console.WriteLine(markDownFile);
 
     if (markDownFile.CodeSnippetsCount * 2 != count)
@@ -47,10 +54,37 @@ var stats = markDownFiles
     })
     .ToList();
 
-Console.WriteLine("Overall stats:");
+Console.WriteLine("\nOverall stats:");
 Console.WriteLine($" Unique codesnippet types found: {stats.Count}");
 string statsAsString = " " + String.Join("\n  ", stats
-    .Select(stat => $"{stat.TotalCount, 4}x found in {stat.NumFilesOccurences, 3} file(s): {stat.CodeSnippetType}"));
+    .Select(stat => $"{stat.TotalCount,4}x found in {stat.NumFilesOccurences,3} file(s): {stat.CodeSnippetType}"));
 
 Console.WriteLine($" {statsAsString}");
 
+Arguments ParseArguments(string[] args)
+{
+    if (args.Length == 0)
+    {
+        Console.WriteLine("Error in arguments!");
+        Console.WriteLine("Syntax: <program> <rootFolderToScan> [filePattern = *.md] [-onlyTopLevel = no]");
+        Console.WriteLine("  For example:");
+        Console.WriteLine("    ScanMarkDownFiles /sources/myrepo");
+        Console.WriteLine("      will scan all /sources/myrepo including subdirs for *.md files");
+        Console.WriteLine("");
+        Console.WriteLine("    ScanMarkDownFiles /sources/myrepo *.mdwn");
+        Console.WriteLine("      will scan all /sources/myrepo including subdirs for *.mdwn files");
+        Console.WriteLine("");
+        Console.WriteLine("    ScanMarkDownFiles /sources/myrepo *.md -onlyTopLevel");
+        Console.WriteLine("      will only scan the top level directory /sources/myrepo for *.md files");
+        Environment.Exit(1);
+    }
+
+    Arguments arguments = new Arguments();
+
+    arguments.RootFolder = args[0];
+
+    if (args.Length >= 2) arguments.FilePattern = args[1];
+    if (args.Length == 3) arguments.OnlyToplevel = (args[2].ToLower().Trim() == "-onlytoplevel");
+
+     return arguments;
+}
